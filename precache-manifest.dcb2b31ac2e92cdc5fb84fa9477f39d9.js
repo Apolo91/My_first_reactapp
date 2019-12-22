dc3a04a761cf0b7ed8fb45fe1a33868e156e6381
andrey <A_polo91@mail.ru>
2019-12-22 15:43:21 +0300

    Updates

---
+++ precache-manifest.dcb2b31ac2e92cdc5fb84fa9477f39d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169aee414a21e3a66dc7daa52c0e42ba",
+    "url": "/My_first_reactapp/index.html"
+  },
+  {
+    "revision": "1e12533fa8d4c266cfb9",
+    "url": "/My_first_reactapp/static/css/main.fc17e9c4.chunk.css"
+  },
+  {
+    "revision": "a70d80faaf1bc360e36e",
+    "url": "/My_first_reactapp/static/js/2.20a7853c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/My_first_reactapp/static/js/2.20a7853c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e12533fa8d4c266cfb9",
+    "url": "/My_first_reactapp/static/js/main.772f6a82.chunk.js"
+  },
+  {
+    "revision": "27025b8bcfff91ed62dc",
+    "url": "/My_first_reactapp/static/js/runtime-main.fc9db729.js"
+  }
+]);
